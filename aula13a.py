b21f2eb5ade01626cff139d55e865fc6375d4e04
Danilo Rodrigues de Oliveira <danilo.rodrigues38@gmail.com>
2022-12-12 20:20:44 -0200

    algumas alterações

--- aula13a.py
+++ aula13a.py
@@ -1,14 +1,15 @@
-# n1 = int(input('Digite um número: '))
-# for c in range(1, 11):
-#     print('{} x {:2} = {:2}'.format(n1, c, n1 * c))  # Taboada
+n1 = int(input('Digite um número: '))
+for c in range(1, 11):
+    print('{:2} x {} = {:2}'.format(c, n1, n1 * c))  # Taboada
+print('\n')
 
 for i in range(1, 6):
     print(i, end=' ')
-print('FIM')
+print('FIM\n')
 
 for c in range(6, 0, -1):
     print(c, end=' ')
-print('FIM')
+print('FIM\n')
 
 
 
