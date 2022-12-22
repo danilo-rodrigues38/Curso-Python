af5b809afeb5fab7472644bfcc04bd6c1774815d
Danilo Rodrigues de Oliveira <danilo.rodrigues38@gmail.com>
2022-12-22 19:54:22 -0200

    Update ex101.py

--- Exercícios/ex101.py
+++ Exercícios/ex101.py
@@ -2,8 +2,20 @@
 # ano de nascimento de uma pessoa, retornando um valor literal indicando se uma pessoa
 # tem voto NEGADO, OPCIONAL e OBRIGATÓRIO nas eleições.
 
-def voto():
+from datetime import date
 
+def voto(data):
+    idade = date.today().year - data
+    print(f'Com {idade} anos e ', end='')
+    if idade < 16:
+        return 'NEGADO'
+    elif idade >= 18 and idade <= 65:
+        return 'OBRIGATÓRIO'
+    else:
+        return 'OPCIONAL'
 
 
 # Programa principal
+nasc = int(input('Em que ano você nasceu: '))
+id = voto(nasc)
+print(f'o voto é {id}')
