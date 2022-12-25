8e1ef6a978d76696ee9b7e409290b1a2b595bf6f
Danilo Rodrigues de Oliveira <danilo.rodrigues38@gmail.com>
2022-12-25 12:36:07 -0200

    Atualizando o ex106a.py conforme resolução do professor Gustavo Guanabara

--- Exercícios/ex106a.py
+++ Exercícios/ex106a.py
@@ -11,19 +11,19 @@ c = ('\033[m',        # 0 = Sem cores
 
 def ajuda(com):
     titulo(f'Acessando o manual do comando \'{com}\'', 4)
-    print(c[6], end=' ')
+    print(c[6], end='')
     help(com)
-    print(c[0], end=' ')
+    print(c[0], end='')
     sleep(2)
 
 
 def titulo(msg, cor=0):
     tam = len(msg) + 4
-    print(c[cor], end=' ')
+    print(c[cor], end='')
     print('~' * tam)
     print(f'{msg}'.center(tam))
     print('~' * tam)
-    print(c[0], end=' ')
+    print(c[0], end='')
     sleep(1)
 
 
