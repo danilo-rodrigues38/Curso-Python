5a7c9f0537610c93383f7aa8ac3cf4084741cdfd
Danilo Rodrigues de Oliveira <danilo.rodrigues38@gmail.com>
2022-12-27 12:02:51 -0300

    Add ex113.py

---
+++ Exercícios/ex114.py
@@ -0,0 +1,9 @@
+import urllib
+import urllib.request
+
+try:
+    site = urllib.request.urlopen('http://www.pudim.com.br')
+except:
+    print('\n\033[31mO site pudim não está acessível no momento.\033[m')
+else:
+    print('\n\033[32mConsegui acessar o site Pudim com sucesso!\033[m')
